1154079437622aeae44fdbca67c2c7b7a32ac8bf
Harshvardhan Pathak <hpathak@twitter.com>
2021-03-03 17:39:01 +0800

    Parsing SearchResponse, using ZonedDateTime

--- search-tweets-java-core/src/main/java/dev/twitter/api/v2/model/Attachments.java
+++ search-tweets-java-core/src/main/java/dev/twitter/api/v2/model/Attachments.java
@@ -2,31 +2,10 @@ package dev.twitter.api.v2.model;
 
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class Attachments {
   private List<String> pollIds;
   private List<String> mediaKeys;
-
-  public List<String> getPollIds() {
-    return pollIds;
-  }
-
-  public void setPollIds(List<String> pollIds) {
-    this.pollIds = pollIds;
-  }
-
-  public List<String> getMediaKeys() {
-    return mediaKeys;
-  }
-
-  public void setMediaKeys(List<String> mediaKeys) {
-    this.mediaKeys = mediaKeys;
-  }
-
-  @Override
-  public String toString() {
-    return "Attachment{" +
-        "pollIds=" + pollIds +
-        ", mediaKeys=" + mediaKeys +
-        '}';
-  }
 }
